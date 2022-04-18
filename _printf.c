9d7804713bf5c9868e82a721850b0f7400f5620a
WangariM <wangariminyu@gmail.com>
2022-04-18 20:38:22 +0400

    Done

--- _printf.c
+++ _printf.c
@@ -1,211 +1,83 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdint.h>
+
 /**
- * _withformat4 - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
+ * check_format - checks if there is a valid format specifier
+ * @format: possible valid format specifier
+ * Return: pointer to valid function or NULL
  */
-int _withformat4(char c, int count, va_list valist)
+int (*check_format(const char *format))(va_list)
 {
-	uintptr_t p;
-	void *pi;
+ int i = 0;
+ print_t p[] = {
+					{"c", print_c},
+					{"s", print_s},
+					{"i", print_i},
+					{"d", print_d},
+					{"b", print_b},
+					{"u", print_u},
+					{"o", print_o},
+					{"x", print_x},
+					{"X", print_X},
+					{"p", print_p},
+					{"S", print_S},
+					{"r", print_r},
+					{"R", print_R},
+					{NULL, NULL}};
 
-
-	switch (c)
-	{
-	case 'p':
-		pi = va_arg(valist, void *);
-		p = (uintptr_t)pi;
-
-		if (pi == NULL)
-		{
-			_printf("(nil)");
-			count += 5;
-		}
-		else
-		{
-			_putchar('0');
-			_putchar('x');
-			count += 2;
-			count += print_hl(p);
-		}
-		break;
-	default:
-		count += 2;
-		_putchar('%');
-		_putchar(c);
-	}
-	return (count);
+ for (; p[i].t != NULL; i++)
+ {
+		if (*(p[i].t) == *format)
+			break;
+ }
+ return (p[i].f);
 }
 
 /**
- * _withformat3 - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-int _withformat3(char c, int count, va_list valist)
-{
-	char *s;
-	int i;
-	char si[6] = "(null)";
-
-	switch (c)
-	{
-	case 'R':
-		s = va_arg(valist, char *);
-		if (!s)
-		{
-			for (i = 0; si[i]; i++, count++)
-				_putchar(si[i]);
-		}
-		else
-			count += rot13(s);
-		break;
-	case 'r':
-		s = va_arg(valist, char *);
-		if (!s)
-		{
-			for (i = 0; si[i]; i++, count++)
-				_putchar(si[i]);
-		}
-		else
-			count += print_rev(s);
-		break;
-		default:
-			count = _withformat4(c, count, valist);
-	}
-	return (count);
-}
-/**
- * _withformat2 - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-int _withformat2(char c, int count, va_list valist)
-{
-	unsigned int k;
-
-	switch (c)
-	{
-		case 'b':
-			k = va_arg(valist, unsigned int);
-
-			count += print_bi(k);
-			break;
-		case 'o':
-			k = va_arg(valist, unsigned int);
-
-			count += print_octal(k);
-			break;
-		case 'x':
-			k = va_arg(valist, unsigned int);
-
-			count += print_hexalow(k);
-			break;
-		case 'X':
-			k = va_arg(valist, unsigned int);
-
-			count += print_hexaup(k);
-			break;
-		case 'u':
-			k = va_arg(valist, unsigned int);
-
-			count += print_unsig(k);
-			break;
-		default:
-			count = _withformat3(c, count, valist);
-	}
-	return (count);
-}
-
-
-/**
- * _withformat - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-
-int _withformat(char c, int count, va_list valist)
-{
-	int j, i;
-	char *s;
-	char si[6] = "(null)";
-
-	switch (c)
-	{
-		case 'c':
-			j = va_arg(valist, int);
-			count += _putchar(j);
-			break;
-		case 's':
-			s = va_arg(valist, char *);
-			if (!s)
-			{
-				for (i = 0; si[i]; i++, count++)
-					_putchar(si[i]);
-			}
-			else
-				count += _printstring(s);
-			break;
-		case '%':
-			count += _putchar('%');
-			break;
-		case 'i':
-		case 'd':
-			j = va_arg(valist, int);
-
-			if (!j)
-			{
-				count++;
-				_putchar('0');
-			} else
-				count += print_number(j);
-			break;
-		default:
-			count = _withformat2(c, count, valist);
-	}
-	return (count);
-}
-
-/**
- * _printf - Fuction that prints to the std output
- * @format: list of parameters passed
- * Return: @count the number of characters printed
+ * _printf - function for format printing
+ * @format: list of arguments to printing
+ * Return: Number of characters to printing
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	va_list valist;
+ va_list ap;
+ int (*f)(va_list);
+ unsigned int i = 0, counter = 0;
 
-	va_start(valist, format);
-
-	if (!format)
+ if (format == NULL)
 		return (-1);
-	for (i = 0; format[i]; i++)
-	{
+
+ va_start(ap, format);
+ while (format && format[i])
+ {
 		if (format[i] != '%')
 		{
-			count++;
 			_putchar(format[i]);
-		}
-		else if (format[i + 1])
-		{
+			counter++;
 			i++;
-			count = _withformat(format[i], count, valist);
+			continue;
 		}
 		else
-			return (-1);
-	}
-	va_end(valist);
-	return (count);
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				counter++;
+				i += 2;
+				continue;
+			}
+			else
+			{
+				f = check_format(&format[i + 1]);
+				if (f == NULL)
+					return (-1);
+				i += 2;
+				counter += f(ap);
+				continue;
+			}
+		}
+		i++;
+ }
+ va_end(ap);
+ return (counter);
 }
